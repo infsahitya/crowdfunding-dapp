120cc0c702b376180e2e53a1a73b1d7debb42a41
infsahitya <sahitya.api@gmail.com>
2024-05-22 19:41:33 +0530

    update:

--- server/test/crowdfunding.e2e-spec.ts
+++ server/test/crowdfunding.e2e-spec.ts
@@ -8,23 +8,24 @@ const tokens = (n: number) => hre.ethers.parseUnits(n.toString(), "ether");
 
 const dummyCampaign = {
   endDateTime: "0000",
-  minimumGoalAmount: 123,
-  targetGoalAmount: 23456,
   title: "Example Campaign",
+  targetGoalAmount: tokens(2222),
+  minimumGoalAmount: tokens(1111),
   description: "Some description",
   thumbnailURI: "http://google.com",
 } as const;
 
 const EventIdentifiers = {
   createCampaign: "CreateCampaign",
+  contributeToCampaign: "ContributeToCampaign",
 } as const;
 
 describe("Crowdfunding Platform", () => {
-  let deployer: any, otherUser: any;
+  let deployer: any, user1: any, user2: any;
   let app: Crowdfunding, idGenerator: IDGenerator;
 
   beforeEach(async () => {
-    [deployer, otherUser] = await hre.ethers.getSigners();
+    [deployer, user1, user2] = await hre.ethers.getSigners();
 
     const IDGenerator: ContractFactory =
       await hre.ethers.getContractFactory("IDGenerator");
@@ -91,6 +92,46 @@ describe("Crowdfunding Platform", () => {
     it("Match Public Campaigns Count", async () => {
       const data = await app.getPublicCampaigns();
       expect(data.length).to.be.equal(0);
+    });
+  });
+
+  describe("Contribute Money To Campaign", () => {
+    let transaction: any, receipt: any, logs: any[];
+    const donationAmount = tokens(20);
+
+    beforeEach(async () => {
+      transaction = await app
+        .connect(user1)
+        .createCampaign(
+          dummyCampaign.title,
+          dummyCampaign.endDateTime,
+          dummyCampaign.description,
+          dummyCampaign.thumbnailURI,
+          dummyCampaign.targetGoalAmount,
+          dummyCampaign.minimumGoalAmount,
+        );
+
+      receipt = await transaction.wait();
+      logs = receipt?.logs.filter(
+        (log: any) => log.fragment.name === EventIdentifiers.createCampaign,
+      );
+
+      transaction = await app
+        .connect(user2)
+        .contributeToCampaign(logs[0].args._campaign.id, {
+          value: donationAmount,
+        });
+      await transaction.wait();
+    });
+
+    it("Check Raised Amount", async () => {
+      const data = await app.getCampaign(logs[0].args._campaign.id);
+      expect(data.raisedAmount).to.be.equal(donationAmount);
+    });
+
+    it("Check Contract Holdings", async () => {
+      const data = await hre.ethers.provider.getBalance(app.getAddress());
+      expect(data).to.be.equal(donationAmount);
     });
   });
 });
