da961258dc219715727c0e9ae12ab4e056e6a109
Shagun Dubey <1202shagun@gmail.com>
2024-05-24 23:43:41 +0530

    update

--- client/src/pages/campagin-detail/index.tsx
+++ client/src/pages/campagin-detail/index.tsx
@@ -1,8 +1,11 @@
 import { Covid_Img } from "@/assets";
 import { Progress } from "@/components/ui/progress";
+import { BiInfoCircle } from "react-icons/bi";
 import { FaEthereum } from "react-icons/fa";
 import { IoIosTimer } from "react-icons/io";
 import { IoWalletOutline } from "react-icons/io5";
+import { TiInfoOutline } from "react-icons/ti";
+import { Checkbox } from "@/components/ui/checkbox"
 
 export default function __campaignDetail() {
   return (
@@ -102,19 +105,46 @@ export default function __campaignDetail() {
 
 
           {/* -------left side------- */}
-          <div className=" w-[50%] px-4 text-gray-300">
-            <h3 className=" text-lg font-medium mb-4 text-white">Current status of campaign</h3>
+          <div className=" w-[50%] pl-6 pr-4 text-gray-300">
+            {/* ---------------- */}
+            <div className=" mb-10">
+              <h3 className=" text-lg font-medium mb-4 text-white">Current status of campaign</h3>
 
-            <div className=" bg-[#343A40] rounded-lg p-6">
-              <h3 className=" text-xl font-medium mb-4">Campaign Balance</h3>
-              <p>Amount stored in smart contract.</p>
-              <div className="flex mb-1 items-center justify-between gap-5">
-                <Progress value={33} className=" h-2"/>
-                <p className="inline-block font-semibold text-primary whitespace-nowrap leading-tight rounded-xl text-lg">
-                 33%
-                </p>
+              <div className=" bg-[#343A40] rounded-lg p-6">
+                <h3 className=" text-xl font-medium mb-4">Campaign Balance</h3>
+                <p>Amount stored in smart contract.</p>
+                <div className="flex mb-1 items-center justify-between gap-5">
+                  <Progress value={33} className=" h-2"/>
+                  <p className="inline-block font-semibold text-primary whitespace-nowrap leading-tight rounded-xl text-lg">
+                  33%
+                  </p>
+                </div>
+                <p>0 ETH funded by 0 backers</p>
               </div>
-              <p>0 ETH funded by 0 backers</p>
+            </div>
+
+            {/* --------withdraw-------- */}
+            <div>
+              <h3 className=" text-lg font-medium mb-4 text-white">Withdraw</h3>
+
+              <div className=" p-6 bg-[#343A40] rounded-lg">
+                  <h2 className=" text-lg font-bold text-white mb-3" >Withdraw Raised Funds</h2>
+
+                  <div className=" p-4 rounded-md bg-blue-200 flex items-center gap-3 mb-3">
+                    <BiInfoCircle className=" text-blue-800" size={20}/>
+                    <span className=" text-base text-blue-800">To withdraw raised funds, campaign has to be <span className=" font-bold">ended</span></span>
+                  </div>
+
+                  <div className=" p-4 rounded-md bg-orange-200 flex items-center gap-3 mb-6">
+                    <TiInfoOutline className=" text-orange-800" size={20}/>
+                    <Checkbox className=" border-orange-800"/>
+                    <span className=" text-base text-orange-800">I/We undestand that, once campaign has ended it cannot be reversed.</span>
+                  </div>
+
+                  <button className=" w-full p-3 rounded-md bg-red-600 uppercase text-white font-medium shadow-md">
+                    End Campaign & Withdraw
+                  </button>
+              </div>
             </div>
           </div>
         </div>
