9f81c3bce5624e1ce87b75e433c71b6a09e586b8
Shagun Dubey <1202shagun@gmail.com>
2024-05-25 00:52:00 +0530

    update

--- client/src/pages/create-campagin/index.tsx
+++ client/src/pages/create-campagin/index.tsx
@@ -1,3 +1,4 @@
+import { FaEthereum } from "react-icons/fa";
 import { MdCampaign } from "react-icons/md";
 
 export default function __createCampaign() {
@@ -13,12 +14,22 @@ export default function __createCampaign() {
         </div>
       </div>
 
-      <div className=" flex ">
+      <div className=" flex text-white">
         {/* right-side */}
         <div className=" w-1/2 py-6 flex flex-col gap-4">
           <div className="flex flex-col text-white ">
             <label className="leading-loose">Campaign Title</label>
             <input type="text" className="px-4 py-4 border focus:ring-primary focus:border-primary w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-300 placeholder:text-gray-300 bg-background" placeholder="Campaign title" />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="leading-loose text-white">Min. Amount</label>
+            <div className="relative focus-within:text-gray-300 text-gray-400">
+              <input type="text" className="pr-4 pl-10 px-4 py-4 border focus:ring-primary focus:border-primary w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-300 placeholder:text-gray-300 bg-background" placeholder="0.001ETH" />
+              <div className="absolute left-4 top-[16px]">
+                <FaEthereum className=" text-gray-500 text-xl"/>
+              </div>
+            </div>
           </div>
         </div>
 
