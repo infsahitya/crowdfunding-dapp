e4a68270064fb0d60696b9b39b2d6c5dfcd3ce62
infsahitya <sahitya.api@gmail.com>
2024-05-31 01:48:45 +0530

    update

---
+++ client/src/hooks/useCampaignDetails.tsx
@@ -0,0 +1,28 @@
+import useAppStore from "@/store/app.store";
+import { useEffect, useState } from "react";
+export default function useCampaignDetails(id: string) {
+  const app = useAppStore((state) => state.getApp());
+  const [campaignDetails, setCampaignDetails] = useState<CampaignProps>(
+    {} as CampaignProps,
+  );
+
+  useEffect(() => {
+    app.getCampaign(id).then((campaign: CampaignProps) => {
+      setCampaignDetails(() => ({
+        id: campaign.id,
+        title: campaign.title,
+        status: campaign.status,
+        donors: campaign.donors,
+        creator: campaign.creator,
+        endDateTime: campaign.endDateTime,
+        description: campaign.description,
+        thumbnailURI: campaign.thumbnailURI,
+        raisedAmount: campaign.raisedAmount,
+        targetGoalAmount: campaign.targetGoalAmount,
+        minimumGoalAmount: campaign.minimumGoalAmount,
+      }));
+    });
+  }, []); // eslint-disable-line
+
+  return { campaignDetails } as const;
+}
