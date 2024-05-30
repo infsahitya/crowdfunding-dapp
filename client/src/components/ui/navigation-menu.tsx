a5da9539da8d19068be4027ec4d5cb683278f7ed
infsahitya <sahitya.api@gmail.com>
2024-05-30 13:09:48 +0530

    update

--- client/src/components/ui/navigation-menu.tsx
+++ client/src/components/ui/navigation-menu.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
-import { cva } from "class-variance-authority";
 import { ChevronDown } from "lucide-react";
+import { cva } from "class-variance-authority";
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 
 import { cn } from "@/lib/utils";
 
@@ -116,13 +116,13 @@ NavigationMenuIndicator.displayName =
   NavigationMenuPrimitive.Indicator.displayName;
 
 export {
-  navigationMenuTriggerStyle,
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuContent,
   NavigationMenuTrigger,
-  NavigationMenuLink,
-  NavigationMenuIndicator,
   NavigationMenuViewport,
+  NavigationMenuIndicator,
+  navigationMenuTriggerStyle,
 };
