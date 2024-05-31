8e389de89c88a2fd7842015768ce939a9d40d516
infsahitya <sahitya.api@gmail.com>
2024-05-31 12:38:51 +0530

    update

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,18 +1,14 @@
-import { usePublicCampaigns } from "@/hooks";
 import Footer from "@/components/base/Footer";
 import HeroSection from "@/components/base/HeroSection";
 import { Subscribe } from "@/components/base/Subscribe";
-import CampaingCard from "@/components/base/CampaignCard";
 
 export default function __home() {
-  const { allCampaigns } = usePublicCampaigns();
-
   return (
     <div className=" text-gray-300 relative w-full">
       <HeroSection />
 
       <div className=" relative w-full">
-        <div className=" w-full flex justify-center items-center pt-8 pb-20">
+        {/* <div className=" w-full flex justify-center items-center pt-8 pb-20">
           <h1 className=" text-4xl text-center font-semibold">
             Take part in
             <br /> our <span className=" text-primary">Active</span> Campaign
@@ -22,7 +18,7 @@ export default function __home() {
           {allCampaigns.map((campaign, index) => (
             <CampaingCard key={index} {...campaign} />
           ))}
-        </div>
+        </div> */}
 
         <Subscribe />
         <Footer />
--- client/src/pages/my-campaigns/index.tsx
+++ client/src/pages/my-campaigns/index.tsx
@@ -1,14 +1,23 @@
+import { useMyCampaigns } from "@/hooks";
 import CampaingCard from "@/components/base/CampaignCard";
-import { usePublicCampaigns } from "@/hooks";
 
 export default function __myCampaigns() {
-  const { allCampaigns } = usePublicCampaigns();
+  const { myCampaigns } = useMyCampaigns();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 xl:gap-12 w-full px-[4rem] xl:px-[9rem]">
-      {allCampaigns.map((campaign, index) => (
-        <CampaingCard key={index} {...campaign} />
-      ))}
+    <div className="relative py-10">
+      <div className="relative w-full flex justify-center items-center pt-8 pb-20">
+        <h1 className=" text-4xl text-center font-semibold">
+          My
+          <br />
+          <span className=" text-primary">Campaign</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 xl:gap-12 w-full px-[4rem] xl:px-[9rem]">
+        {myCampaigns.map((campaign, index) => (
+          <CampaingCard key={index} {...campaign} />
+        ))}
+      </div>
     </div>
   );
 }
