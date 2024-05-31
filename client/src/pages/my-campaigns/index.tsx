2492468752410a8b2323f2c48a06742b0bf2c5a5
infsahitya <sahitya.api@gmail.com>
2024-05-31 12:57:46 +0530

    update

--- client/src/pages/my-campaigns/index.tsx
+++ client/src/pages/my-campaigns/index.tsx
@@ -7,10 +7,10 @@ export default function __myCampaigns() {
   return (
     <div className="relative py-10">
       <div className="relative w-full flex justify-center items-center pt-8 pb-20">
-        <h1 className=" text-4xl text-center font-semibold">
+        <h1 className=" text-4xl text-center font-semibold text-white">
           All My
           <br />
-          <span className=" text-primary">Campaign</span>
+          <span className="text-primary">Campaign</span>
         </h1>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 xl:gap-12 w-full px-[4rem] xl:px-[9rem]">
